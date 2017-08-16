262ace0effe63ffd91a65f4b99762dc94447b0a1
yiling <yiling.chen@gmail.com>
2017-08-16 02:56:39 +0000

    create necessary folder when setup

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 sudo ansible-playbook -i "localhost," -c local package.yml
 
+if [ ! -d '../credit_show' ]; then
+    mkdir ../credit_show
+fi
+
+if [ ! -d '../mops_show' ]; then
+    mkdir ../mops_show
+fi
+
+if [ ! -d '../xml' ]; then
+    mkdir ../xml
+fi
+
+if [ ! -d '../json' ]; then
+    mkdir ../json
+fi
+
